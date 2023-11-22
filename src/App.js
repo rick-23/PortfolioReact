54800f2a977c71f356b10370ce75b89241ad2362
rick-23 <swamynathan.radhakrishnan@gmail.com>
2023-11-22 12:18:00 -0500

    Added Projects Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import Header from "./components/header/header";
 import Intro from "./components/intro/intro";
 import About from "./components/about/about";
 import Contact from "./components/contact/contact";
-// import Projects from "./components/projects/projects";
+import Projects from "./components/projects/projects";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Intro/>
-      <About/>
-      {/* <Projects/> */}
-      <Contact/>
+      <Header />
+      <Intro />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import projects from '../../projects.json';
 
-const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+const Projects = () => (
+  <section className="bg-primary text-white px-5 py-32" id="projects">
+    <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+      <div className="about-info mb-5">
+        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-yellow-300 pb-2">
+          Projects
+        </h2>
+
+        <p className="pb-5">
+          These are some of my projects across my areas of interest.
+        </p>
+      </div>
+
+      <div className="about-img"></div>
+    </div>
+
+    <div className="projects container mx-auto grid md:grid-cols-2 gap-10">
+      {projects.map((project, i) => {
+        return (
+          <div className="flex flex-col max-w-xs p-6 bg-primary border border-gray-200 m-3 rounded-lg hover:text-[yellow] hover:border-[yellow]">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">{project.title}</h5>
+            <p className="font-normal">Tech stack: {project.technologies}</p>
+          </div>
+        );
+      })}
+    </div>
+  </section>
+);
 
 export default Projects
